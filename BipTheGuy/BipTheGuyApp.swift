343ae3bbd5d4a135c58bd8288109051b1bafc5f4
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-04 17:46:49 +0100

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Dmitry Sharabin on 4.1.23..
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
